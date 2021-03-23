47bf09ae72c09a1bb7dc35a3b910f4cb17691b6e
Vittokm <vittokm@gmail.com>
2021-03-23 17:02:32 +0100

    small changes in css

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let count = 0;
+let num;
+let num2; 
+let nums = [];
+
+function pick(num) {
+    
+    let res = document.querySelector("#res");
+    res.innerHTML += num;
+    count++;
+
+    
+    
+}
